8d0b91d696a5155d7b1a99a5858c17f35c25174f
Lex Borisoff <lex@borisoff.dev>
2024-03-22 20:56:33 -0400

    updated directories files

--- local.example/directories.sh
+++ local.example/directories.sh
@@ -1,31 +1,7 @@
 #!/usr/bin/usr bash
 
-dev() {
-	if command -v sd &>/dev/null; then
-		sd -root=~/dev "$@"
-	fi
-}
-
 terminal() {
 	if command -v sd &>/dev/null; then
 		sd -root=~/.terminal "$@"
-	fi
-}
-
-desktop() {
-	if command -v sd &>/dev/null; then
-		sd -root=~/Desktop
-	fi
-}
-
-desk() {
-	if command -v sd &>/dev/null; then
-		desktop
-	fi
-}
-
-docs() {
-	if command -v sd &>/dev/null; then
-		sd -root=~/Documents
 	fi
 }
