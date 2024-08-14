9a91a0f515dea0a6de7e754b8770f664fc6a5b1d
Lex <lex@borisoff.dev>
2024-08-14 08:44:40 -0400

    added inputrc file to copy-profile script

--- functions/copy-profile.sh
+++ functions/copy-profile.sh
@@ -2,10 +2,15 @@
 
 profile_dir=/etc/profile.d/
 terminal_dir=~/.terminal/git-bash/profile
+inputrc=~/.inputrc
 
 copy-profile() {
 	if test -d $terminal_dir; then
 		cp $profile_dir/git-prompt.sh $profile_dir/aliases.sh $terminal_dir
+
+		if test -f $inputrc; then
+			cp $inputrc $terminal_dir
+		fi
 	else
 		echo '"'$terminal_dir'"' does not exist
 	fi
