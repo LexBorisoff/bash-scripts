d3a8a9df024835bfaa7b7d4eff5ee8bc35bc0e10
Lex <lex@borisoff.dev>
2024-08-15 11:47:59 -0400

    updated web script to have aliases instead of functions

--- functions/web.sh
+++ functions/web.sh
@@ -1,31 +1,7 @@
 #!/usr/bin/env bash
 
-mdn() {
-	if command -v web &>/dev/null; then
-		web --engine=mdn "$@"
-	fi
-}
-
-npmjs() {
-	if command -v web &>/dev/null; then
-		web --engine=npm "$@"
-	fi
-}
-
-github() {
-	if command -v web &>/dev/null; then
-		web --engine=github "$@"
-	fi
-}
-
-ff() {
-	if command -v web &>/dev/null; then
-		web --browser=firefox "$@"
-	fi
-}
-
-yt() {
-	if command -v web &>/dev/null; then
-		web --engine=youtube "$@"
-	fi
-}
+alias mdn='web --engine=mdn'
+alias npmjs='web --engine=npm'
+alias github='web --engine=github'
+alias yt='web --engine=youtube'
+alias ff='web --browser=firefox'
