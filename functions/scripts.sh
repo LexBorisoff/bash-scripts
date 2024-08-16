fc8dca6be7206cb797161493fff4b544efdc523e
Lex Borisoff <lex@borisoff.dev>
2024-08-15 22:16:42 -0400

    created scripts.sh

---
+++ functions/scripts.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/usr bash
+
+__scripts() {
+	local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+	local parent_dir="$(dirname "$script_dir")"
+
+	if command -v sd &>/dev/null; then
+		sd -root=$parent_dir "$@"
+	fi
+}
+
+__open_scripts() {
+	local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+	local parent_dir="$(dirname "$script_dir")"
+
+	code $parent_dir
+}
