9a15e3f5cdcac83299f593d02de9bf05256a366a
Lex Borisoff <lex@borisoff.dev>
2024-08-15 22:16:19 -0400

    updated source-functions file

--- source-functions.sh
+++ source-functions.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-functions_dir="$script_dir/functions"
-local_dir="$script_dir/local"
+__source_functions() {
+	local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+	local functions_dir="$script_dir/functions"
+	local local_dir="$script_dir/local"
 
-if [ -d "$functions_dir" ]; then
-	for file in "$functions_dir"/*.sh; do
-		if [ -f "$file" ]; then
-			source "$file"
-		fi
-	done
-fi
+	if [ -d "$functions_dir" ]; then
+		for file in "$functions_dir"/*.sh; do
+			if [ -f "$file" ]; then
+				source "$file"
+			fi
+		done
+	fi
 
-if [ -d "$local_dir" ]; then
-	for file in "$local_dir"/*.sh; do
-		if [ -f "$file" ]; then
-			source "$file"
-		fi
-	done
-fi
+	if [ -d "$local_dir" ]; then
+		for file in "$local_dir"/*.sh; do
+			if [ -f "$file" ]; then
+				source "$file"
+			fi
+		done
+	fi
+}
+
+__source_functions
