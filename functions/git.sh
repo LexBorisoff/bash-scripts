c96bcc957bf68b9d37872dec7e29302c2dfe0bd5
Lex <lex@borisoff.dev>
2024-11-27 14:01:26 -0500

    feat(git): added gl function

--- functions/git.sh
+++ functions/git.sh
@@ -17,11 +17,20 @@ if command -v __git_complete &>/dev/null; then
 	__git_complete gs _git_checkout
 fi
 
-# show branches
+# show/filter branches
 gb() {
 	if ! test -z "$1"; then
 		git branch -vv --color='always' | grep "$1" --color='always'
 	else
 		git branch -vv
+	fi
+}
+
+# show/filter logs
+gl() {
+	if ! test -z "$1"; then
+		git log --grep "$1"
+	else
+		git log
 	fi
 }
