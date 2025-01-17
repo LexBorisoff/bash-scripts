b14bcc838475d1fc5acb3a77ca1a0a18382298bb
Lex Borisoff <lex@borisoff.dev>
2025-01-17 09:54:48 -0500

    updated web aliases

--- bash/scripts/web.sh
+++ bash/scripts/web.sh
@@ -1,8 +1,11 @@
-alias mdn='web --engine=mdn'
-alias npmjs='web --engine=npm'
-alias github='web --engine=github'
-alias yt='web --engine=youtube'
-alias ff='web --browser=firefox'
+if command -v web &>/dev/null; then
+  alias mdn='web --engine=mdn'
+  alias npmjs='web --engine=npm'
+  alias github='web --engine=github'
+  alias youtube='web --engine=youtube'
+  alias yt='web --engine=youtube'
+  alias ff='web --browser=firefox'
+fi
 
 domain() {
   curl --silent --write-out "%{http_code}\n" "https://www.whois.com/whois/$1" | grep -E "Creation Date:|Registry Expiry Date:"
