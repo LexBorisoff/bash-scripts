bb0568bfc08bdc4a1e768cb59a371c737947cef1
Lex Borisoff <lex@borisoff.dev>
2025-01-20 23:43:26 -0500

    installed search-web if command doesn't exist

--- bash/scripts/web.sh
+++ bash/scripts/web.sh
@@ -1,11 +1,15 @@
-if command -v web &>/dev/null; then
-  alias mdn='web --engine=mdn'
-  alias npmjs='web --engine=npm'
-  alias github='web --engine=github'
-  alias youtube='web --engine=youtube'
-  alias yt='web --engine=youtube'
-  alias ff='web --browser=firefox'
+if ! command -v web &>/dev/null; then
+  echo Installing \"search-web\" ...
+  npm i -g search-web &>/dev/null
+  echo \"search-web\" installed
 fi
+
+alias mdn='web --engine=mdn'
+alias npmjs='web --engine=npm'
+alias github='web --engine=github'
+alias youtube='web --engine=youtube'
+alias yt='web --engine=youtube'
+alias ff='web --browser=firefox'
 
 domain() {
   curl --silent --write-out "%{http_code}\n" "https://www.whois.com/whois/$1" | grep -E "Creation Date:|Registry Expiry Date:"
