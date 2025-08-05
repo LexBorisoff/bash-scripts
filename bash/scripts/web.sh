b50f18f84c38a43e372c69cd3e8ed875c721d267
Lex Borisoff <lex@borisoff.dev>
2025-08-05 00:01:39 -0400

    deleted domain function

--- bash/scripts/web.sh
+++ bash/scripts/web.sh
@@ -6,11 +6,3 @@ if command -v web &>/dev/null; then
 	alias yt='web --engine=youtube'
 	alias ff='web --browser=firefox'
 fi
-
-domain() {
-	paste -d ' ' <(printf "Created On:\nExpires On:\nUpdated On:") <(
-		curl --silent -L "https://www.whois.com/whois/$1" |
-			grep -E "Registered On:|Expires On:" |
-			grep -Eo '[0-9]{4}-[0-9]{2}-[0-9]{2}'
-	)
-}
